1ce264d0fabecb4e44024002768c5a8b2130efd9
rossdavidson1 <rossdavidson80@yahoo.com>
2023-03-11 19:25:28 +0000

    test

--- app.js
+++ app.js
@@ -1,61 +1,36 @@
-customElements.define('page-one', class extends HTMLElement {
+customElements.define('page-home', class extends HTMLElement {
     connectedCallback() {
         this.innerHTML = `
             <ion-header>
-                <ion-toolbar color="danger">
-                    <ion-title slot="end" >Page 1</ion-title>
+                <ion-toolbar color="medium">
+                    <ion-title>Home</ion-title>
                 </ion-toolbar>
             </ion-header>
 
             <ion-content padding>
                 <ion-card>
                     <ion-card-header>
-                        <ion-card-title id="title-animal1">Frog</ion-card-title>
+                        <ion-card-title>Choose an Adventure</ion-card-title>
                     </ion-card-header>
 
                     <ion-card-content>
-                        <ion-item>
-                            <ion-label>Hi</ion-label>
-                        </ion-item>
-                        <ion-button expand="block" href="#/two">Go to page two</ion-button>
-                        <ion-button expand="block" href="#/three">Go to page three</ion-button>
+                        <ion-button expand="block" href="#/parks">Find Parks</ion-button>
+                        <ion-button expand="block" href="#/hikes">Find Hikes</ion-button>
+                        <ion-button expand="block" href="#/coasts">Find Coastal Walks</ion-button>
                     </ion-card-content>
                 </ion-card>
             </ion-content>
-        `;
-    }
-});
 
-customElements.define('page-two', class extends HTMLElement {
-    connectedCallback() {
-        this.innerHTML = `
-            <ion-header>
-                <ion-toolbar color="secondary">
-                    <ion-buttons>
-                        <ion-back-button></ion-back-button>
-                    </ion-buttons>
-                    <ion-title>Page 2</ion-title>
+            <ion-footer>
+                <ion-toolbar>
+                    <ion-title>Footer Toolbar</ion-title>
                 </ion-toolbar>
-            </ion-header>
-            <ion-content>
-                <ion-card>
-                    <ion-card-header>
-                        <ion-card-title id="title-animal2">Mantis</ion-card-title>
-                    </ion-card-header>
-
-                    <ion-card-content>
-                        <ion-item>
-                            <ion-label>Hi</ion-label>
-                        </ion-item>
-                        <ion-button expand="block" href="#/three">Go to page three</ion-button>
-                    </ion-card-content>
-                </ion-card>
-            </ion-content>
+            </ion-footer>
         `;
     }
 });
 
-customElements.define('page-three', class extends HTMLElement {
+customElements.define('page-parks', class extends HTMLElement {
     connectedCallback() {
         this.innerHTML = `
             <ion-header>
@@ -63,20 +38,78 @@ customElements.define('page-three', class extends HTMLElement {
                     <ion-buttons>
                         <ion-back-button></ion-back-button>
                     </ion-buttons>
-                    <ion-title>Page 3</ion-title>
+                    <ion-title slot="end">Parks</ion-title>
                 </ion-toolbar>
             </ion-header>
             <ion-content>
                 <ion-card>
                     <ion-card-header>
-                        <ion-card-title id="title-animal3">Cat</ion-card-title>
+                        <ion-card-title>Parks</ion-card-title>
                     </ion-card-header>
 
                     <ion-card-content>
                         <ion-item>
                             <ion-label>Hi</ion-label>
                         </ion-item>
-                        <ion-button expand="block" href="#/">Go to page one</ion-button>
+                        <ion-button expand="block" href="#/three">Go to page three</ion-button>
+                    </ion-card-content>
+                </ion-card>
+            </ion-content>
+        `;
+    }
+});
+
+customElements.define('page-hikes', class extends HTMLElement {
+    connectedCallback() {
+        this.innerHTML = `
+            <ion-header>
+                <ion-toolbar color="danger">
+                    <ion-buttons>
+                        <ion-back-button></ion-back-button>
+                    </ion-buttons>
+                    <ion-title slot="end">Hikes</ion-title>
+                </ion-toolbar>
+            </ion-header>
+            <ion-content>
+                <ion-card>
+                    <ion-card-header>
+                        <ion-card-title>Hikes</ion-card-title>
+                    </ion-card-header>
+
+                    <ion-card-content>
+                        <ion-item>
+                            <ion-label>Hi</ion-label>
+                        </ion-item>
+                        <ion-button expand="block" href="#/three">Go to page three</ion-button>
+                    </ion-card-content>
+                </ion-card>
+            </ion-content>
+        `;
+    }
+});
+
+customElements.define('page-coasts', class extends HTMLElement {
+    connectedCallback() {
+        this.innerHTML = `
+            <ion-header>
+                <ion-toolbar color="secondary">
+                    <ion-buttons>
+                        <ion-back-button></ion-back-button>
+                    </ion-buttons>
+                    <ion-title slot="end">Coastal Walks</ion-title>
+                </ion-toolbar>
+            </ion-header>
+            <ion-content>
+                <ion-card>
+                    <ion-card-header>
+                        <ion-card-title>Coastal Waks</ion-card-title>
+                    </ion-card-header>
+
+                    <ion-card-content>
+                        <ion-item>
+                            <ion-label>Hi</ion-label>
+                        </ion-item>
+                        <ion-button expand="block" href="#/three">Go to page three</ion-button>
                     </ion-card-content>
                 </ion-card>
             </ion-content>
