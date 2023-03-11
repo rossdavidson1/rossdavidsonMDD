682f71f25fc2e3df790fb595947ad264f932a987
rossdavidson1 <rossdavidson80@yahoo.com>
2023-03-11 17:33:49 +0000

    test

---
+++ app.js
@@ -0,0 +1,84 @@
+customElements.define('page-one', class extends HTMLElement {
+    connectedCallback() {
+        this.innerHTML = `
+            <ion-header>
+                <ion-toolbar color="danger">
+                    <ion-title slot="end" >Page 1</ion-title>
+                </ion-toolbar>
+            </ion-header>
+
+            <ion-content padding>
+                <ion-card>
+                    <ion-card-header>
+                        <ion-card-title id="title-animal1">Frog</ion-card-title>
+                    </ion-card-header>
+
+                    <ion-card-content>
+                        <ion-item>
+                            <ion-img id="img-choice" src="./Pictures/frog.jpg"/>
+                        </ion-item>
+                        <ion-button expand="block" href="#/two">Go to page two</ion-button>
+                    </ion-card-content>
+                </ion-card>
+            </ion-content>
+        `;
+    }
+});
+
+customElements.define('page-two', class extends HTMLElement {
+    connectedCallback() {
+        this.innerHTML = `
+            <ion-header>
+                <ion-toolbar color="secondary">
+                    <ion-buttons>
+                        <ion-back-button></ion-back-button>
+                    </ion-buttons>
+                    <ion-title>Page 2</ion-title>
+                </ion-toolbar>
+            </ion-header>
+            <ion-content>
+                <ion-card>
+                    <ion-card-header>
+                        <ion-card-title id="title-animal2">Mantis</ion-card-title>
+                    </ion-card-header>
+
+                    <ion-card-content>
+                        <ion-item>
+                            <ion-img id="img-choice" src="./Pictures/mantis.jpg"/>
+                        </ion-item>
+                        <ion-button expand="block" href="#/three">Go to page three</ion-button>
+                    </ion-card-content>
+                </ion-card>
+            </ion-content>
+        `;
+    }
+});
+
+customElements.define('page-three', class extends HTMLElement {
+    connectedCallback() {
+        this.innerHTML = `
+            <ion-header>
+                <ion-toolbar color="success">
+                    <ion-buttons>
+                        <ion-back-button></ion-back-button>
+                    </ion-buttons>
+                    <ion-title>Page 3</ion-title>
+                </ion-toolbar>
+            </ion-header>
+            <ion-content>
+                <ion-card>
+                    <ion-card-header>
+                        <ion-card-title id="title-animal3">Cat</ion-card-title>
+                    </ion-card-header>
+
+                    <ion-card-content>
+                        <ion-item>
+                            <ion-img id="img-choice" src="./Pictures/cat.jpg"/>
+                        </ion-item>
+                        <ion-button expand="block" href="#/">Go to page one</ion-button>
+                    </ion-card-content>
+                </ion-card>
+            </ion-content>
+        `;
+    }
+});
