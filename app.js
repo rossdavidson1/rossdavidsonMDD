fb216dfd770858b41aa143c5a22b68f4b7db8679
rossdavidson1 <rossdavidson80@yahoo.com>
2023-03-15 21:56:34 +0000

    hikes

--- app.js
+++ app.js
@@ -22,13 +22,12 @@ customElements.define('page-home', class extends HTMLElement {
             </ion-content>
 
             <ion-footer>
-          
                 <ion-toolbar color="light">
                     <ion-grid>
                         <ion-row>
                             <ion-col size="5"><ion-button class="navbtns"href="#/"><ion-icon name="home"></ion-icon></ion-button></ion-col>
                             <ion-col size="5"><ion-button href="#/themap"><ion-icon name="navigate"></ion-button></ion-col>
-                            <ion-col size="2"><ion-button href="#/hikes"><ion-icon name="person"></ion-button></ion-col>
+                            <ion-col size="2"><ion-button href="#/profile"><ion-icon name="person"></ion-button></ion-col>
                         </ion-row>
                     </ion-grid>
                 </ion-toolbar>
@@ -62,6 +61,18 @@ customElements.define('page-parks', class extends HTMLElement {
                     </ion-card-content>
                 </ion-card>
             </ion-content>
+
+            <ion-footer>
+            <ion-toolbar color="light">
+                <ion-grid>
+                    <ion-row>
+                        <ion-col size="5"><ion-button class="navbtns"href="#/"><ion-icon name="home"></ion-icon></ion-button></ion-col>
+                        <ion-col size="5"><ion-button href="#/themap"><ion-icon name="navigate"></ion-button></ion-col>
+                        <ion-col size="2"><ion-button href="#/profile"><ion-icon name="person"></ion-button></ion-col>
+                    </ion-row>
+                </ion-grid>
+            </ion-toolbar>
+            </ion-footer
         `;
     }
 });
@@ -70,7 +81,7 @@ customElements.define('page-hikes', class extends HTMLElement {
     connectedCallback() {
         this.innerHTML = `
             <ion-header>
-                <ion-toolbar color="danger">
+                <ion-toolbar color="dark">
                     <ion-buttons>
                         <ion-back-button></ion-back-button>
                     </ion-buttons>
@@ -85,12 +96,149 @@ customElements.define('page-hikes', class extends HTMLElement {
 
                     <ion-card-content>
                         <ion-item>
+                            <ion-label>Choose Difficulty</ion-label>
+                        </ion-item>
+                        <ion-button expand="block" color="success" href="#/hikes/easy">Easy</ion-button>
+                        <ion-button expand="block" color="warning" href="#/hikes/medium">Medium</ion-button>
+                        <ion-button expand="block" color="danger" href="#/hikes/hard">Hard</ion-button>
+                    </ion-card-content>
+                </ion-card>
+            </ion-content>
+
+            <ion-footer>
+            <ion-toolbar color="light">
+                <ion-grid>
+                    <ion-row>
+                        <ion-col size="5"><ion-button class="navbtns"href="#/"><ion-icon name="home"></ion-icon></ion-button></ion-col>
+                        <ion-col size="5"><ion-button href="#/themap"><ion-icon name="navigate"></ion-button></ion-col>
+                        <ion-col size="2"><ion-button href="#/profile"><ion-icon name="person"></ion-button></ion-col>
+                    </ion-row>
+                </ion-grid>
+            </ion-toolbar>
+            </ion-footer
+        `;
+    }
+});
+
+customElements.define('page-easy-hikes', class extends HTMLElement {
+    connectedCallback() {
+        this.innerHTML = `
+            <ion-header>
+                <ion-toolbar color="success">
+                    <ion-buttons>
+                        <ion-back-button></ion-back-button>
+                    </ion-buttons>
+                    <ion-title slot="end">Easy Hikes</ion-title>
+                </ion-toolbar>
+            </ion-header>
+            <ion-content>
+                <ion-card>
+                    <ion-card-header>
+                        <ion-card-title><h1>Easy Hikes</h1></ion-card-title>
+                    </ion-card-header>
+
+                    <ion-card-content>
+                        <ion-item>
                             <ion-label>Hi</ion-label>
                         </ion-item>
                         <ion-button expand="block" href="#/three">Go to page three</ion-button>
                     </ion-card-content>
                 </ion-card>
             </ion-content>
+
+            <ion-footer>
+            <ion-toolbar color="light">
+                <ion-grid>
+                    <ion-row>
+                        <ion-col size="5"><ion-button class="navbtns"href="#/"><ion-icon name="home"></ion-icon></ion-button></ion-col>
+                        <ion-col size="5"><ion-button href="#/themap"><ion-icon name="navigate"></ion-button></ion-col>
+                        <ion-col size="2"><ion-button href="#/profile"><ion-icon name="person"></ion-button></ion-col>
+                    </ion-row>
+                </ion-grid>
+            </ion-toolbar>
+            </ion-footer
+        `;
+    }
+});
+
+customElements.define('page-medium-hikes', class extends HTMLElement {
+    connectedCallback() {
+        this.innerHTML = `
+            <ion-header>
+                <ion-toolbar color="warning">
+                    <ion-buttons>
+                        <ion-back-button></ion-back-button>
+                    </ion-buttons>
+                    <ion-title slot="end">Medium Hikes</ion-title>
+                </ion-toolbar>
+            </ion-header>
+            <ion-content>
+                <ion-card>
+                    <ion-card-header>
+                        <ion-card-title><h1>Medium Hikes</h1></ion-card-title>
+                    </ion-card-header>
+
+                    <ion-card-content>
+                        <ion-item>
+                            <ion-label>Hi</ion-label>
+                        </ion-item>
+                        <ion-button expand="block" href="#/three">Go to page three</ion-button>
+                    </ion-card-content>
+                </ion-card>
+            </ion-content>
+
+            <ion-footer>
+            <ion-toolbar color="light">
+                <ion-grid>
+                    <ion-row>
+                        <ion-col size="5"><ion-button class="navbtns"href="#/"><ion-icon name="home"></ion-icon></ion-button></ion-col>
+                        <ion-col size="5"><ion-button href="#/themap"><ion-icon name="navigate"></ion-button></ion-col>
+                        <ion-col size="2"><ion-button href="#/profile"><ion-icon name="person"></ion-button></ion-col>
+                    </ion-row>
+                </ion-grid>
+            </ion-toolbar>
+            </ion-footer
+        `;
+    }
+});
+
+customElements.define('page-hard-hikes', class extends HTMLElement {
+    connectedCallback() {
+        this.innerHTML = `
+            <ion-header>
+                <ion-toolbar color="danger">
+                    <ion-buttons>
+                        <ion-back-button></ion-back-button>
+                    </ion-buttons>
+                    <ion-title slot="end">Hard Hikes</ion-title>
+                </ion-toolbar>
+            </ion-header>
+            <ion-content>
+                <ion-card>
+                    <ion-card-header>
+                        <ion-card-title><h1>Hard Hikes</h1></ion-card-title>
+                    </ion-card-header>
+
+                    <ion-card-content>
+                        <ion-item>
+                            <ion-label>Hi</ion-label>
+                        </ion-item>
+                        <ion-button expand="block" href="#/three">Go to page three</ion-button>
+                    </ion-card-content>
+                </ion-card>
+            </ion-content>
+
+            <ion-footer>
+            <ion-toolbar color="light">
+                <ion-grid>
+                    <ion-row>
+                        <ion-col size="5"><ion-button class="navbtns"href="#/"><ion-icon name="home"></ion-icon></ion-button></ion-col>
+                        <ion-col size="5"><ion-button href="#/themap"><ion-icon name="navigate"></ion-button></ion-col>
+                        <ion-col size="2"><ion-button href="#/profile"><ion-icon name="person"></ion-button></ion-col>
+                    </ion-row>
+                </ion-grid>
+            </ion-toolbar>
+            </ion-footer
         `;
     }
 });
@@ -120,6 +268,18 @@ customElements.define('page-coasts', class extends HTMLElement {
                     </ion-card-content>
                 </ion-card>
             </ion-content>
+
+            <ion-footer>
+            <ion-toolbar color="light">
+                <ion-grid>
+                    <ion-row>
+                        <ion-col size="5"><ion-button class="navbtns"href="#/"><ion-icon name="home"></ion-icon></ion-button></ion-col>
+                        <ion-col size="5"><ion-button href="#/themap"><ion-icon name="navigate"></ion-button></ion-col>
+                        <ion-col size="2"><ion-button href="#/profile"><ion-icon name="person"></ion-button></ion-col>
+                    </ion-row>
+                </ion-grid>
+            </ion-toolbar>
+            </ion-footer
         `;
     }
 });
@@ -129,10 +289,7 @@ customElements.define('page-map', class extends HTMLElement {
         this.innerHTML = `
             <ion-header>
                 <ion-toolbar color="secondary">
-                    <ion-buttons>
-                        <ion-back-button></ion-back-button>
-                    </ion-buttons>
-                    <ion-title slot="end">Map</ion-title>
+                    <ion-title slot="start">Map</ion-title>
                 </ion-toolbar>
             </ion-header>
             <ion-content>
@@ -145,10 +302,58 @@ customElements.define('page-map', class extends HTMLElement {
                         <ion-item>
                             <ion-label>Hi</ion-label>
                         </ion-item>
-                        <ion-button expand="block" href="#/three">Go to page three</ion-button>
                     </ion-card-content>
                 </ion-card>
             </ion-content>
+
+            <ion-footer>
+            <ion-toolbar color="light">
+                <ion-grid>
+                    <ion-row>
+                        <ion-col size="5"><ion-button class="navbtns"href="#/"><ion-icon name="home"></ion-icon></ion-button></ion-col>
+                        <ion-col size="5"><ion-button href="#/themap"><ion-icon name="navigate"></ion-button></ion-col>
+                        <ion-col size="2"><ion-button href="#/profile"><ion-icon name="person"></ion-button></ion-col>
+                    </ion-row>
+                </ion-grid>
+            </ion-toolbar>
+            </ion-footer
+        `;
+    }
+});
+
+customElements.define('page-profile', class extends HTMLElement {
+    connectedCallback() {
+        this.innerHTML = `
+            <ion-header>
+                <ion-toolbar color="warning">
+                    <ion-title slot="start">Profile</ion-title>
+                </ion-toolbar>
+            </ion-header>
+            <ion-content>
+                <ion-card>
+                    <ion-card-header>
+                        <ion-card-title><h1>Coastal Waks</h1></ion-card-title>
+                    </ion-card-header>
+
+                    <ion-card-content>
+                        <ion-item>
+                            <ion-label>Hi</ion-label>
+                        </ion-item>
+                    </ion-card-content>
+                </ion-card>
+            </ion-content>
+
+            <ion-footer>
+            <ion-toolbar color="light">
+                <ion-grid>
+                    <ion-row>
+                        <ion-col size="5"><ion-button class="navbtns"href="#/"><ion-icon name="home"></ion-icon></ion-button></ion-col>
+                        <ion-col size="5"><ion-button href="#/themap"><ion-icon name="navigate"></ion-button></ion-col>
+                        <ion-col size="2"><ion-button href="#/profile"><ion-icon name="person"></ion-button></ion-col>
+                    </ion-row>
+                </ion-grid>
+            </ion-toolbar>
+            </ion-footer
         `;
     }
 });
