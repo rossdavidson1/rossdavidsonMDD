79d24cdc5507fb82842b6e103b097f63e8f66e77
rossdavidson1 <rossdavidson80@yahoo.com>
2023-03-22 13:19:21 +0000

    medium

--- app.js
+++ app.js
@@ -162,6 +162,48 @@ customElements.define('page-easy-hikes', class extends HTMLElement {
     }
 });
 
+customElements.define('page-medium-hikes', class extends HTMLElement {
+    connectedCallback() {
+        this.innerHTML = `
+            <ion-header>
+                <ion-toolbar color="warning">
+                    <ion-buttons>
+                        <ion-back-button></ion-back-button>
+                    </ion-buttons>
+                    <ion-title slot="end">Medium Hikes</ion-title>
+                </ion-toolbar>
+            </ion-header>
+            <ion-content padding>                
+                <ion-list>
+                    ${mhikes.map(mhike => `
+                        <ion-item button onclick="showMDetail('${mhike.title}')">
+                            <ion-avatar slot="start">
+                                <img src="${mhike.avatar}">
+                            </ion-avatar>
+                            <ion-label>
+                                <h1>${mhike.title}</h1>
+                            </ion-label>
+                        </ion-item>
+                    `).join('\n')}
+                
+                </ion-list>   
+            </ion-content>
+
+            <ion-footer>
+            <ion-toolbar color="light">
+                <ion-grid>
+                    <ion-row>
+                        <ion-col size="5"><ion-button class="navbtns"href="#/"><ion-icon name="home"></ion-icon></ion-button></ion-col>
+                        <ion-col size="5"><ion-button href="#/themap"><ion-icon name="navigate"></ion-button></ion-col>
+                        <ion-col size="2"><ion-button href="#/profile"><ion-icon name="person"></ion-button></ion-col>
+                    </ion-row>
+                </ion-grid>
+            </ion-toolbar>
+            </ion-footer
+        `;
+    }
+});
+
 const nav = document.querySelector('ion-nav');
 
 
@@ -174,6 +216,17 @@ function showEDetail(eTitle) {
     }
     console.log(ehike.title)
     nav.push('easy-hike-detail', { ehike });
+}
+
+function showMDetail(mTitle) {
+    let mhike = new Object;
+    for (anmhike of mhikes) {
+        if (anmhike.title === mTitle) {
+            mhike = anmhike;
+        }
+    }
+    console.log(mhike.title)
+    nav.push('medium-hike-detail', { mhike });
 }
 
 customElements.define('easy-hike-detail', class extends HTMLElement {
@@ -226,34 +279,42 @@ customElements.define('easy-hike-detail', class extends HTMLElement {
     }
 });
 
-
-customElements.define('page-medium-hikes', class extends HTMLElement {
+customElements.define('medium-hike-detail', class extends HTMLElement {
     connectedCallback() {
         this.innerHTML = `
-            <ion-header>
-                <ion-toolbar color="warning">
-                    <ion-buttons>
-                        <ion-back-button></ion-back-button>
-                    </ion-buttons>
-                    <ion-title slot="end">Medium Hikes</ion-title>
-                </ion-toolbar>
-            </ion-header>
-            <ion-content>
-                <ion-card>
-                    <ion-card-header>
-                        <ion-card-title><h1>Medium Hikes</h1></ion-card-title>
-                    </ion-card-header>
+        <ion-header >
+        <ion-toolbar color="warning">
+            <ion-buttons slot="start">
+                <ion-back-button defaultHref="/"></ion-back-button>
+            </ion-buttons>
+            <ion-title>${this.mhike.title}
+            </ion-title>
+        </ion-toolbar>
+    </ion-header>
+    <ion-content fullscreen class="ion-padding">
+        <ion-card>
+            <ion-card-content>
+                <ion-item>
+                    <ion-img id="img-choice" src="${this.mhike.image}"/>
+                </ion-item>
+            </ion-card-content>
+        </ion-card>
+        <ion-card>
+            <ion-card-content>
+                <ion-item>
+                    <h1>${this.mhike.title}</h1>
+                </ion-item>
+                <ion-item>
+                    <ion-label>${this.mhike.distance}</ion-label>
+                </ion-item>
+                <ion-item>
+                    <p>${this.mhike.description}</p>
+                </ion-item>
+            </ion-card-content>
+        </ion-card>
+    </ion-content>
 
-                    <ion-card-content>
-                        <ion-item>
-                            <ion-label>Hi</ion-label>
-                        </ion-item>
-                        <ion-button expand="block" href="#/three">Go to page three</ion-button>
-                    </ion-card-content>
-                </ion-card>
-            </ion-content>
-
-            <ion-footer>
+    <ion-footer>
             <ion-toolbar color="light">
                 <ion-grid>
                     <ion-row>
@@ -267,6 +328,7 @@ customElements.define('page-medium-hikes', class extends HTMLElement {
         `;
     }
 });
+
 
 customElements.define('page-hard-hikes', class extends HTMLElement {
     connectedCallback() {
--- data.js
+++ data.js
@@ -35,3 +35,41 @@ let ehikes = [
         'description': 'Get to know this 2.1-km circular trail near Aberdeen, Aberdeen. Generally considered an easy route, it takes an average of 34 min to complete. This is a popular trail for mountain biking, running, and walking, but you can still enjoy some solitude during quieter times of day. The trail is open year-round and is beautiful to visit anytime.'
     },
 ];
+
+let mhikes = [
+    {
+        'title': 'Countesswells Circular',
+        'avatar': './Routes/countesswellsRoute.png',
+        'distance': '6.9km',
+        'image': './Pictures/countesswells.jpg',
+        'description': 'Get to know this 6.9-km circular trail near Aberdeen, Aberdeen. Generally considered a moderately challenging route, it takes an average of 1 h 56 min to complete. This is a popular trail for hiking, mountain biking, and running, but you can still enjoy some solitude during quieter times of day.'
+    },
+    {
+        'title': 'Elsick Mounth',
+        'avatar': './Routes/elsickRoute.png',
+        'distance': '13.5km',
+        'image': './Pictures/elsick.jpg',
+        'description': 'Explore this 13.5-km out-and-back trail near Peterculter, Aberdeen. Generally considered a moderately challenging route, it takes an average of 3 h 50 min to complete. This trail is great for hiking, running, and walking, and it is unlikely you will encounter many other people while exploring.'
+    },
+    {
+        'title': 'Kincorth Hill',
+        'avatar': './Routes/kincorthRoute.png',
+        'distance': '2.7km',
+        'image': './Pictures/kincorthHill.jpg',
+        'description': 'Discover this 2.7-km circular trail near Aberdeen, Aberdeen. Generally considered a moderately challenging route, it takes an average of 39 min to complete. This trail is great for hiking and walking, and it is unlikely you will encounter many other people while exploring.'
+    },
+    {
+        'title': 'Kingshill Wood Circular',
+        'avatar': './Routes/kingshillRoute.png',
+        'distance': '7.7km',
+        'image': './Pictures/kingshill.jpg',
+        'description': 'Head out on this 7.7-km circular trail near Aberdeen, Aberdeen. Generally considered a moderately challenging route, it takes an average of 2 h 3 min to complete. This is a popular trail for hiking, mountain biking, and running, but you can still enjoy some solitude during quieter times of day.'
+    },
+    {
+        'title': 'Den of Maidencraig Circular',
+        'avatar': './Routes/maidencraigRoute.png',
+        'distance': '2.1km',
+        'image': './Pictures/maidencraig.jpg',
+        'description': 'Experience this 2.1-km circular trail near Aberdeen, Aberdeen. Generally considered a moderately challenging route, it takes an average of 31 min to complete. This trail is great for running and walking, and it is unlikely you will encounter many other people while exploring.'
+    },
+];
